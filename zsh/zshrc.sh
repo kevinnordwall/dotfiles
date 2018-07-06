8afb16434e7e6616300c8c497629a40f48456b1c
Kevin Nordwall <kevinno@kth.se>
2018-07-06 18:57:02 +0200

    new email

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -3,19 +3,16 @@ POWERLEVEL9K_PROMPT_ON_NEWLINE=true
 POWERLEVEL9K_RPROMPT_ON_NEWLINE=true
 POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
 POWERLEVEL9K_MODE='nerdfont-complete'
+
+# Imports
 source  ~/powerlevel9k/powerlevel9k.zsh-theme
-
-# cd + ls
-c() {
-	cd $1;
-	ls;
-}
-alias cd="c"
-
-# Open finder by simply writing f in terminal
-f() {
-	open .;
-}
+source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/history.zsh
+source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/key-bindings.zsh
+source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/completion.zsh
+source ~/dotfiles/zsh/plugins/vi-mode.plugin.zsh
+source ~/dotfiles/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/dotfiles/zsh/keybindings.sh 
 
 # Starts tmux on launch if session is not in tmux already
 if [ "$TMUX" = "" ]; then tmux; fi
